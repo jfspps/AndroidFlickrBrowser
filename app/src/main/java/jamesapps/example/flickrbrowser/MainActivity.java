88284ba1aaffa79a61fd6380b54240d09d979480
jfspps <jfsapps@gmail.com>
2020-12-21 16:13:56 +0000

    updated gitignore

--- app/src/main/java/jamesapps/example/flickrbrowser/MainActivity.java
+++ app/src/main/java/jamesapps/example/flickrbrowser/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Overrided
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
